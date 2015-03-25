6019facd6eb2af6fd1ae886843ad0702c210e17d
Alvaro Denis Acosta Quesada <adacosta@CIAA.uci.cu>
2015-03-25 02:18:38 -0400

    First commit

---
+++ cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/storage/WORK/COCO/cocos2d-x-3.2/tests/SavingLasVegas/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml : \
+/storage/WORK/COCO/cocos2d-x-3.2/tests/SavingLasVegas/cocos2d/cocos/platform/android/java/AndroidManifest.xml \
---
+++ cocos2d/cocos/platform/android/java/gen/R.java.d
@@ -0,0 +1,1 @@
+ : /storage/WORK/COCO/cocos2d-x-3.2/tests/SavingLasVegas/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml \
